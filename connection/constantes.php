ecebdd294d6f81aa666f3dfd7b734fa317b16633
Jaime <jaime.ornu@gmail.com>
2023-10-18 23:15:33 +0200

    Datos Buscaminas default en constantes

--- connection/constantes.php
+++ connection/constantes.php
@@ -1,8 +1,13 @@
-<?php 
+<?php
 
-class Constantes {
+class Constantes
+{
     static $host = 'localhost';
     static $user = 'jornu';
     static $password = 'Chubaca2077';
     static $bd = 'desafio_buscaminas';
+
+//- Datos Buscaminas default -----------------------------------------------------
+    static $tam = 10;
+    static $min = 2;
 }
